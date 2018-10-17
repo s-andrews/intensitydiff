f79f6e7f5dd8757b8272dd838172415dd66e98b6
s-andrews <simon.andrews@babraham.ac.uk>
2018-10-17 16:12:57 +0100

    Intial code import

---
+++ R/intensity_difference.R
@@ -0,0 +1,51 @@
+#' Intensity difference test
+#'
+#' This function calculates an intensity corrected z-score
+#' statistic for a matched pair of numeric vectors. For each
+#' point a local sub-sampling of the data is performed where
+#' a set of points with the most similar mean values across
+#' the two datasets are collected.  The distribution of
+#' differences for these selected points is then modelled with
+#' a normal distribution and a z-score value for the point
+#' being tested is then calculated, along with the p.value
+#' corresponding to that z-score.  The absolute difference
+#' and FDR corrected p-values are also reported.
+#'
+#' @param values.1 A numeric vector of values
+#' @param values.2 A numeric vector of values
+#' @export
+#' @examples
+#' intensity.difference()
+
+intensity.difference <- function (values.1,values.2) {
+  average.values <- (values.1+values.2)/2
+
+  order(average.values) -> sorted.indices
+
+  order(sorted.indices) -> reverse.lookup
+
+  sapply(1:length(values.1), function(x) {
+    start <- reverse.lookup[x]-250
+    if (start < 0) start <- 0
+    end <- start+500
+    if (end > length(values.1)) {
+      end <- length(values.1)
+      start <- end-500
+    }
+
+    local.diffs <- values.1[sorted.indices[start:end]]-values.2[sorted.indices[start:end]]
+
+    # We assume a mean of 0 and calculate the sd
+    sqrt(mean(local.diffs*local.diffs)) -> local.sd
+
+    # Now we work out the p.value for the value we're actually
+    # looking at in the context of this distibution
+    pnorm(values.1[x]-values.2[x],mean=0,sd=local.sd) -> local.p
+
+    if (local.p >0.5) local.p <- (1 - local.p)
+
+    return (local.p)
+
+  }
+  )
+}
